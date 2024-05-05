116f05869a42be20b93b192d6fea333bbbfd0230
Sam Rotthier <sam.rotthier@student.kdg.be>
2024-05-05 16:24:35 +0200

    US-36 changeQuantityOfIngredient implemented - to be tested

--- src/main/java/be/kdg/sa/warehouse/controller/IngredientController.java
+++ src/main/java/be/kdg/sa/warehouse/controller/IngredientController.java
@@ -3,13 +3,15 @@ package be.kdg.sa.warehouse.controller;
 import be.kdg.sa.warehouse.controller.dto.IngredientDto;
 import be.kdg.sa.warehouse.domain.Ingredient;
 import be.kdg.sa.warehouse.services.IngredientService;
+import jdk.jfr.Frequency;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/ingredients")
@@ -31,8 +33,18 @@ public class IngredientController {
             List<IngredientDto> ingredientDtos = allOrders.stream().map(this::convertToIngredientDto).toList();
             return ResponseEntity.ok(ingredientDtos);
         }
-
     }
+
+    @PatchMapping("/{id}/quantity")
+    public ResponseEntity<?> changeQuantityOfIngredient(@PathVariable UUID id, @RequestParam double Quantity, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return ResponseEntity.badRequest().body("No quantity could be updated");
+        }
+        ingredientService.changeQuantityOfIngredient(id,Quantity);
+        return ResponseEntity.status(HttpStatus.CREATED).body("The Quantity was changed to " + Quantity);
+    }
+
+
 
     private IngredientDto convertToIngredientDto(Ingredient ingredient){
         return new IngredientDto(
--- src/main/java/be/kdg/sa/warehouse/services/IngredientService.java
+++ src/main/java/be/kdg/sa/warehouse/services/IngredientService.java
@@ -2,10 +2,13 @@ package be.kdg.sa.warehouse.services;
 
 import be.kdg.sa.warehouse.domain.Ingredient;
 import be.kdg.sa.warehouse.repositories.IngredientRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class IngredientService {
@@ -20,5 +23,11 @@ public class IngredientService {
 
     public List<Ingredient> getAllIngredients() {
         return ingredientRepository.findAll();
+    }
+
+    @Transactional
+    public void changeQuantityOfIngredient (UUID ingredientId, double Quantity){
+        Ingredient ingredient = ingredientRepository.findIngredientByIngredientId(ingredientId);
+        ingredient.setingredientQuantity(Quantity);
     }
 }
